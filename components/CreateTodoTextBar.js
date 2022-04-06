5de65c18f186c2a1b5c696e58353505abd8fdc04
Horacio <horacinis@gmail.com>
2022-04-06 22:02:05 +1000

    Created post request to create todos

--- components/CreateTodoTextBar.js
+++ components/CreateTodoTextBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 const CreateTodoTextBar = () => {
-  const handleAddTodo = (e) => {
+  const TODOS_URL = 'api/v1/todos';
+
+  const handleAddTodo = async (e) => {
     e.preventDefault();
-    console.log('Todo Added!');
+    try {
+      const todo = { name: e.target.elements.todoName.value };
+      const response = await fetch(TODOS_URL, {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json()
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <form onSubmit={handleAddTodo}>
-      <input type='text'></input>
+      <input type='text' id='todoName'></input>
       <button type='submit'>Add</button>
 
     </form>
--- components/Todo.js
+++ components/Todo.js
@@ -4,9 +4,9 @@ const Todo = ({ todo: { name, priority, done } }) => {
   return (
     <li>
       <input type='checkbox' id={name} checked={Boolean(done)} onChange={() => console.log('chupalo')} />
-      {done ? 'Done' : 'Not done'}
-      <label htmlFor={name}>{name}</label>
-      {priority ? 'Not Priority' : 'Priority'}
+      {done ? ' *Done* ' : ' *Not done* '}
+      <label htmlFor={name}> <strong>{name}</strong></label>
+      {priority ? ' *Not Priority* ' : ' *Priority* '}
       <button>Delete</button>
     </li >
   )
