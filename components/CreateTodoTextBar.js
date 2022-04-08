75b9cd318fbef0261f2c632b825469f224d14c4d
Horacinis <horacinis@gmail.com>
2022-04-08 10:07:58 +1000

    Both add todo text bar and button will be desabled until post request for creating a new todo is successful (before another one can be created), they will be enabled back again if request is not successful so that it can be retried.

--- components/CreateTodoTextBar.js
+++ components/CreateTodoTextBar.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const CreateTodoTextBar = ({ todos, setTodos }) => {
   const TODOS_URL = 'api/v1/todos';
+  const [textInput, setTextInput] = React.useState('');
+  const [disabledFields, setDisabledFields] = React.useState(false);
 
   const handleAddTodo = async (e) => {
     e.preventDefault();
     try {
-      const todo = { name: e.target.elements.todoName.value };
+      setDisabledFields(true)
+      // const todo = { name: e.target.elements.todoName.value }; // to be removed
+      const todo = { name: textInput };
       const response = await fetch(TODOS_URL, {
         method: 'POST',
         body: JSON.stringify(todo),
@@ -16,16 +20,21 @@ const CreateTodoTextBar = ({ todos, setTodos }) => {
       });
       const data = await response.json()
       setTodos([...todos, { ...data.data.todo }]);
-      e.target.elements.todoName.value = '';
+      // e.target.elements.todoName.value = ''; // to be removed
+      setTextInput('');
+      setDisabledFields(false)
     } catch (err) {
+      setDisabledFields(false)
+      alert('No internet connection. Your request could not be sent');
       console.log('Error!', err);
     }
   }
 
+
   return (
     <form onSubmit={handleAddTodo}>
-      <input type='text' id='todoName' />
-      <button type='submit'>Add</button>
+      <input type='text' id='todoName' disabled={disabledFields} onChange={(e) => setTextInput(e.target.value)} value={textInput} />
+      <button disabled={disabledFields} type='submit'>Add</button>
     </form>
   )
 }
