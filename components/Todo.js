6ef005d3431040d04fa5ec49919096af329eeb3e
Horacio <horacinis@gmail.com>
2022-04-11 14:01:32 +1000

    Moved all of the tods methods into a separate file in utils folder

--- components/Todo.js
+++ components/Todo.js
@@ -1,85 +1,24 @@
 import React, { useState, useRef } from 'react';
+import { handleChangeTodoProp, handleDeleteTodo } from '../utils/manageTodosMethods';
 
 const Todo = ({ todo, todos, setTodos }) => {
-  const { _id, name, priority, done } = todo
-  const TODOS_URL = 'api/v1/todos';
+  const { name, priority, done } = todo
   const [editable, setEditable] = useState(false);
   const [newTodoName, setNewTodoName] = useState(name);
   const editableInput = useRef(name);
 
-  const handleDeleteTodo = async () => {
-    try {
-      await fetch(`${TODOS_URL}/${_id}`, {
-        method: 'DELETE'
-      });
-      const updatedTodos = todos.filter((selectedTodo) => selectedTodo._id !== _id);
-      setTodos(updatedTodos);
-    } catch (err) {
-      console.log('Error!', err);
-    }
-  }
-
-  const handleChangeTodoProp = async (todoProp) => {
-    console.log('chupalo')
-    try {
-      const updatedTodos = todos.map((obj) => {
-        if (obj._id === _id) {
-          return { ...obj, [todoProp]: !obj[todoProp] }
-        }
-        return obj;
-      });
-
-      await fetch(`${TODOS_URL}/${_id}`, {
-        method: "PATCH",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ ...todo, [todoProp]: !todo[todoProp] })
-      })
-      // .then(response => { console.log(response.status); return response.json(); })
-      // .then(data => console.log(data.data.todo));
-      setTodos(updatedTodos);
-    } catch (err) {
-      console.log('Error', err);
-    }
-  }
-
-  const handleChangeTodoName = async (todoProp, todoPropValue) => {
-    try {
-      const updatedTodos = todos.map((obj) => {
-        if (obj._id === _id) {
-          return { ...obj, [todoProp]: todoPropValue }
-        }
-        return obj;
-      });
-
-      await fetch(`${TODOS_URL}/${_id}`, {
-        method: "PATCH",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ ...todo, [todoProp]: todoPropValue })
-      })
-      // .then(response => { console.log(response.status); return response.json(); })
-      // .then(data => console.log(data.data.todo));
-      setTodos(updatedTodos);
-    } catch (err) {
-      console.log('Error', err);
-    }
-  }
-
-  const handleEditTodoNameTextInput = () => {
-    setNewTodoName(editableInput.current.value);
-  }
-
   return (
     <li>
       <p>
-        <input type='checkbox' id={name} checked={done} onChange={() => handleChangeTodoProp('done')} />
+        <input type='checkbox' id={name} checked={done} onChange={() => handleChangeTodoProp(todos, todo, setTodos, 'done')} />
 
         {!editable &&
           (<label htmlFor={name}> <strong>{name}</strong></label>)
           ||
-          (<input onChange={handleEditTodoNameTextInput} autoFocus ref={editableInput} type='text' value={newTodoName} />)
+          (<input onChange={() => setNewTodoName(editableInput.current.value)} autoFocus ref={editableInput} type='text' value={newTodoName} />)
         }
 
-        <span onClick={() => handleChangeTodoProp('priority')}>{priority ? ' *Not Priority* ' : ' *Priority* '}</span>
+        <span onClick={() => handleChangeTodoProp(todos, todo, setTodos, 'priority')}>{priority ? ' *Not Priority* ' : ' *Priority* '}</span>
 
         {!editable && (
           <button onClick={() => {
@@ -89,12 +28,12 @@ const Todo = ({ todo, todos, setTodos }) => {
         ) || (
             <button onClick={() => {
               setEditable(!editable)
-              handleChangeTodoName('name', newTodoName)
+              handleChangeTodoProp(todos, todo, setTodos, 'name', newTodoName)
             }}>Save</button>
           )
         }
 
-        <button onClick={() => handleDeleteTodo(todo)}>Delete</button>
+        <button onClick={() => handleDeleteTodo(todos, todo, setTodos)}>Delete</button>
       </p>
     </li>
   )
---
+++ utils/manageTodosMethods.js
@@ -0,0 +1,55 @@
+
+const TODOS_URL = 'api/v1/todos';
+
+export const handleDeleteTodo = async (todosArr, todoItem, setState) => {
+  try {
+    await fetch(`${TODOS_URL}/${todoItem._id}`, {
+      method: 'DELETE'
+    });
+    const updatedTodos = todosArr.filter((selectedTodo) => selectedTodo._id !== todoItem['_id']);
+    setState(updatedTodos);
+  } catch (err) {
+    console.log('Error!', err);
+  }
+}
+
+export const handleChangeTodoProp = async (todosArr, todoItem, setState, ...args) => {
+  try {
+    const [todoProp, todoPropValue] = args;
+
+    if (args.length === 2) {
+      const updatedTodos = todosArr.map((obj) => {
+        if (obj._id === todoItem._id) {
+          return { ...obj, [todoProp]: todoPropValue }
+        }
+        return obj;
+      });
+      await fetch(`${TODOS_URL}/${todoItem._id}`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ ...todoItem, [todoProp]: todoPropValue })
+      })
+      // .then(response => { console.log(response.status); return response.json(); })
+      // .then(data => console.log(data.data.todo));
+      setState(updatedTodos);
+    }
+    if (args.length === 1) {
+      const updatedTodos = todosArr.map((obj) => {
+        if (obj._id === todoItem._id) {
+          return { ...obj, [todoProp]: !obj[todoProp] }
+        }
+        return obj;
+      });
+      await fetch(`${TODOS_URL}/${todoItem._id}`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ ...todoItem, [todoProp]: !todoItem[todoProp] })
+      })
+      // .then(response => { console.log(response.status); return response.json(); })
+      // .then(data => console.log(data.data.todo));
+      setState(updatedTodos);
+    }
+  } catch (err) {
+    console.log('Error', err);
+  }
+}
